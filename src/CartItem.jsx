60d653e7018fae0cd9278452a0ea5fe9736b98ab
bmtonga <mtongabernard6@gmail.com>
2026-06-26 09:59:10 +0200

    State management using Redux

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import {updateQuantity} from './CartSlice';
+import {removeItem} from './CartSlice';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
